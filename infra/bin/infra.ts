e6449562c776342dd55ba1cf4aaaf47d7da8c1d5
Ramin Ismayilov <ramismayilov@gmail.com>
2022-11-11 14:12:09 +0400

    create new stack

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -2,8 +2,13 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { InfraStack } from '../lib/infra-stack';
+import { PipelineStack } from "../lib/pipeline-stack";
 
 const app = new cdk.App();
 new InfraStack(app, 'InfraStack', {
+    env: {account: '670614417011', region: 'eu-central-1'},
+});
+
+new PipelineStack(app, 'PipelineStack', {
     env: {account: '670614417011', region: 'eu-central-1'},
 });

8156e5ed74c36cad0902fdf6169c8082ae37bf0f
Ramin Ismayilov <ramismayilov@gmail.com>
2022-11-11 14:08:30 +0400

    add pipeline stack

---
+++ infra/lib/pipeline-stack.ts
@@ -0,0 +1,64 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { Artifact, IStage, Pipeline } from "aws-cdk-lib/aws-codepipeline";
+import {
+    CloudFormationCreateUpdateStackAction,
+    CodeBuildAction,
+    CodeBuildActionType,
+    GitHubSourceAction,
+} from "aws-cdk-lib/aws-codepipeline-actions";
+import {
+    BuildEnvironmentVariableType,
+    BuildSpec,
+    LinuxBuildImage,
+    PipelineProject,
+} from "aws-cdk-lib/aws-codebuild";
+
+export class PipelineStack extends cdk.Stack {
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        const pipeline = new Pipeline(this, 'Pipeline', {
+            pipelineName: 'MyPipeline',
+            crossAccountKeys: false,
+        });
+
+        const sourceOutput = new Artifact('SourceOutput');
+
+        const sourceAction = new GitHubSourceAction({
+            actionName: 'PipelineSource',
+            owner: 'raminismayilov',
+            repo: 'cdk-serverless-sfn',
+            oauthToken: cdk.SecretValue.secretsManager('github-token'),
+            output: sourceOutput,
+        });
+
+        pipeline.addStage({
+            stageName: 'Source',
+            actions: [sourceAction],
+        });
+
+        const buildOutput = new Artifact('BuildOutput');
+
+        const buildAction = new CodeBuildAction({
+            actionName: "PipelineBuild",
+            input: sourceOutput,
+            outputs: [buildOutput],
+            project: new PipelineProject(this, "BuildProject", {
+                environment: {
+                    buildImage: LinuxBuildImage.STANDARD_5_0,
+                },
+                buildSpec: BuildSpec.fromSourceFilename(
+                    "build-specs/build-spec.yml"
+                ),
+            }),
+        });
+
+        pipeline.addStage({
+            stageName: "Build",
+            actions: [
+                buildAction,
+            ],
+        });
+    }
+}

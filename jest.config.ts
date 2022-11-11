a1ff8d9b78f64a2360a1e178a336a5157bfecc75
Ramin Ismayilov <ramismayilov@gmail.com>
2022-11-11 15:57:18 +0400

    correct app tests matcher

--- jest.config.ts
+++ jest.config.ts
@@ -2,8 +2,8 @@ export default {
     testEnvironment: 'node',
     roots: ['<rootDir>'],
     testMatch: [
-        // 'app/**/tests/*.test.ts',
-        'infra/test/*.test.ts'
+        // '**/app/**/tests/*.test.ts',
+        '**/test/*.test.ts'
     ],
     preset: 'ts-jest',
     transform: {

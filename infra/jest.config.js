8e6d2f9c1ba11bc48a54a38f69347927860ca415
Ramin Ismayilov <ramismayilov@gmail.com>
2022-11-12 18:47:02 +0400

    add test reporting to CodeBuild

--- infra/jest.config.js
+++ infra/jest.config.js
@@ -1,8 +1,18 @@
 module.exports = {
-  testEnvironment: 'node',
-  roots: ['<rootDir>/test'],
-  testMatch: ['**/*.test.ts'],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest'
-  }
+    testEnvironment: 'node',
+    roots: ['<rootDir>/test'],
+    testMatch: ['**/*.test.ts'],
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest',
+    },
+    reporters: [
+        'default',
+        [
+            'jest-junit',
+            {
+                outputDirectory: 'test-reports',
+                outputName: 'test-report.xml',
+            },
+        ],
+    ],
 };

bdeb91741454681dec575e05b75caced47a052a5
Ramin Ismayilov <ramismayilov@gmail.com>
2022-11-12 15:57:15 +0400

    add a pipeline test w/ snapshot

--- infra/test/pipeline.test.ts
+++ infra/test/pipeline.test.ts
@@ -1,19 +1,18 @@
-import { App, Environment } from "aws-cdk-lib";
-import { Match, Template } from "aws-cdk-lib/assertions";
-import { PipelineStack } from "../lib/pipeline-stack";
-import * as Pipeline from "../lib/pipeline-stack";
+import { App, Environment } from 'aws-cdk-lib';
+import { Match, Template } from 'aws-cdk-lib/assertions';
+import { PipelineStack } from '../lib/pipeline-stack';
 
 const testEnv = {
-    account: "123456789012",
-    region: "us-east-1",
-}
+    account: '123456789012',
+    region: 'us-east-1',
+};
 
 test('Pipeline Stack', () => {
-    // const app = new App();
-    //
-    // const stack = new Pipeline.PipelineStack(app, "MyTestStack", {
-    //     env: testEnv,
-    // });
-    //
-    // expect(Template.fromStack(stack).toJSON()).toMatchSnapshot();
+    const app = new App();
+
+    const stack = new PipelineStack(app, 'MyTestStack', {
+        env: testEnv,
+    });
+
+    expect(Template.fromStack(stack).toJSON()).toMatchSnapshot();
 });

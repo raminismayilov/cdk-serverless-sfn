ef66feedc15761b111ae1af28038f0c98152a9bb
Ramin Ismayilov <ramismayilov@gmail.com>
2022-11-13 05:12:45 +0400

    add log

--- app/multiplication/tests/multiplication.test.ts
+++ app/multiplication/tests/multiplication.test.ts
@@ -12,7 +12,7 @@ describe('multiplication', () => {
             credentials: {
                 accessKeyId: 'test',
                 secretAccessKey: 'test',
-            }
+            },
         });
     });
 

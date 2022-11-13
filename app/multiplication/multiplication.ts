9eed0e3a04ee4dcfe32a8d116dc2e4c5b887b3a9
Ramin Ismayilov <ramismayilov@gmail.com>
2022-11-13 04:41:56 +0400

    remove log

--- app/multiplication/multiplication.ts
+++ app/multiplication/multiplication.ts
@@ -4,7 +4,6 @@
  * @returns e.g. { Payload: { product: 3 } }
  */
 export async function handler(event: any): Promise<any> {
-    console.log('Received event:', JSON.stringify(event));
     const { a, b } = event;
     const product = a * b;
     return { product };
